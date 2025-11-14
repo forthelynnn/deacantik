91ac8c54d82e6ef39aa994f85b64e1823fc07b2a
forthelynnn <forthelynnn@gmail.com>
2025-11-15 03:53:20 +0700

    Add UGCGenerator component with initial UI layout

---
+++ components/UGCGenerator.jsx
@@ -0,0 +1,8 @@
+export default function UGCGenerator(){
+  return (
+    <div style={{padding:20,color:'white'}}>
+      <h1>UGC Outfit Generator</h1>
+      <p>Full UI will be implemented here.</p>
+    </div>
+  )
+    }
